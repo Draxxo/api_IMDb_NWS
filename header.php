60feebd7ba4700b75d22386ae0bcdc76f6535d10
Clément <clement@wixiweb.fr>
2018-06-20 15:49:17 +0200

    feat: ajoute le lien de la librairie themoviedb-javascript-library et le fichier api.js au header

--- header.php
+++ header.php
@@ -13,6 +13,9 @@
 	<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" 
 	              integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" 
 	              crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="js/themoviedb.js"></script>
+    <script type="text/javascript" src="js/api.js"></script>
 </head>
 <body>
 
