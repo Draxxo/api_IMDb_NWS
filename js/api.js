be6b317285d233efee653d4b91df65052ebf2c1f
Clément <clement@wixiweb.fr>
2018-06-22 15:19:08 +0200

    feat: ajoute le script d'affichage des films de la page filters
    Avec ajout d'un switch qui récupère la classe du body afin d'exectuer la fonction liée à cette page

--- js/api.js
+++ js/api.js
@@ -1,67 +1,88 @@
 $(document).ready(function () {
 
-    theMovieDb.movies.getPopular({},
-        function (data) {
+    var title = $('body').attr('id');
 
-            let popularFilmObject = JSON.parse(data);
-            console.log(popularFilmObject);
+    switch (title) {
+        case 'filters':
+            cardFilters();
+            break;
+    }
 
-            theMovieDb.genres.getMovieList({language: 'fr-FR'}, function (result) {
-                let genresList = JSON.parse(result).genres;
+    function cardFilters() {
 
-                (function (genre) {
-                    console.log(genre.name, genre.id);
-                })
+        theMovieDb.movies.getPopular({},
+            function (data) {
 
-                popularFilmObject.results.forEach(function (movie) {
-                    let article = $('<article/>', {'class': 'card'});
-                    let cardImage = $('<div/>', {
-                        'class': 'card__image',
-                        'style': 'background-image: url(https://image.tmdb.org/t/p/w500' + movie.poster_path + ')',
-                    });
-                    let cardInfo = $('<div/>', {'class': 'card__info'});
-                    let title = $('<h2/>', {'class': 'title', html: movie.title.substr(0, 20) + '...'});
-                    let genre = $('<p/>', {'class': 'genre', html: movie.genre_ids});
-                    let desc = $('<div/>', {'class': 'desc'});
-                    let rating = $('<div/>', {'class': 'rating'});
-                    let meta = $('<p/>', {'class': 'meta', html: 'jean-claude'});
-                    let stars = $('<div/>', {'class': 'stars'});
-                    let button = $('<a/>', {'class': 'button', html: '► Plus d\'informations'});
-                    let resume = $('<p/>', {html: movie.overview.substr(0, 200) + '...'});
+                let popularFilmObject = JSON.parse(data);
 
-                    let popularity = Math.round(movie.vote_average/2);
-                    console.log(popularity);
+                var genresList;
 
-                    for(let iter = 0; iter < popularity; iter++) {
+                theMovieDb.genres.getMovieList({language: 'fr-FR'}, function (result) {
+                        genresList = JSON.parse(result).genres;
 
-                        let starsImg = $('<i/>', {'class': 'fas fa-star'});
-                        stars.append(starsImg);
+                    }, function (data) {
+                        console.log("Error callback: " + data)
                     }
+                );
 
-                    rating.append(meta);
-                    rating.append(stars);
+                setTimeout(function () {
 
-                    desc.append(rating);
-                    desc.append(resume);
+                    popularFilmObject.results.forEach(function (movie) {
+                        let article = $('<article/>', {'class': 'card'});
+                        let cardImage = $('<div/>', {
+                            'class': 'card__image',
+                            'style': 'background-image: url(https://image.tmdb.org/t/p/w500' + movie.poster_path + ')',
+                        });
+                        let cardInfo = $('<div/>', {'class': 'card__info'});
+                        let title = $('<h2/>', {'class': 'title', html: movie.title.substr(0, 20) + '...'});
+                        let desc = $('<div/>', {'class': 'desc'});
+                        let rating = $('<div/>', {'class': 'rating'});
+                        let stars = $('<div/>', {'class': 'stars'});
+                        let button = $('<a/>', {'class': 'button', html: '► Plus d\'informations'});
+                        let resume = $('<p/>', {html: movie.overview.substr(0, 200) + '...'});
 
-                    cardInfo.append(title);
-                    cardInfo.append(genre);
-                    cardInfo.append(desc);
-                    cardInfo.append(button);
+                        // Compare les genres d'un film avec les genres existant
+                        var genre = $('<p/>', {'class': 'genre', html: 'unknown'});
+                        var meta = $('<p/>', {'class': 'meta', html: 'unknown'});
 
-                    article.append(cardImage);
-                    article.append(cardInfo);
+                        genresList.forEach(function (g) {
 
-                    $('.wrapper').append(article);
-                })
+                            if (g.id == movie.genre_ids[0]) {
+                                genre = $('<p/>', {'class': 'genre', html: g.name});
+                                meta = $('<p/>', {'class': 'meta', html: g.name});
+                            }
+
+                        });
+
+
+                        // Calcul de la note pour afficher un nombre d'étoile
+                        let popularity = Math.round(movie.vote_average / 2);
+
+                        for (let iter = 0; iter < popularity; iter++) {
+                            let starsImg = $('<i/>', {'class': 'fas fa-star'});
+                            stars.append(starsImg);
+                        }
+
+                        rating.append(meta);
+                        rating.append(stars);
+
+                        desc.append(rating);
+                        desc.append(resume);
+
+                        cardInfo.append(title);
+                        cardInfo.append(genre);
+                        cardInfo.append(desc);
+                        cardInfo.append(button);
+
+                        article.append(cardImage);
+                        article.append(cardInfo);
+
+                        $('.wrapper').append(article);
+                    })
+                }, 1000);
 
             }, function (data) {
                 console.log("Error callback: " + data);
             });
-
-
-        }, function (data) {
-            console.log("Error callback: " + data);
-        });
-
+    }
 });
