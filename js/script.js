ac9cf4d0d2f2d992857bddf35e32bcd39c8e94de
Edwin LIENARD <edwin.lienard@outlook.fr>
2018-06-22 11:33:08 +0200

    FEATURE : Ajout Search filters

--- js/script.js
+++ js/script.js
@@ -34,4 +34,12 @@ $( document ).ready(function() {
 	     	}, 500);
 	    });  
 	  });
+      $(document).ready(function(){
+          $("#myInput").on("keyup", function() {
+              var value = $(this).val().toLowerCase();
+              $(".wrapper article").filter(function() {
+                  $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+              });
+          });
+      });
 });
