7b2b0bb23bc1091bfa55778d437412efb1a0589c
Fabrice COTTIN <fabrice.cottin1@viacesi.fr>
2018-06-22 17:01:02 +0200

    inscription qui fonctionne

--- connection.php
+++ connection.php
@@ -16,15 +16,14 @@ if(isset($_POST['email']) && isset($_POST['mdp'])) {
     $password = trim($_POST['mdp']);
 
     $query = 'SELECT * FROM `user` WHERE `email` = :email';
-    $st = $db->prepare($query);
+    $st = $bdd->prepare($query);
     $st->execute(array(':email' => $email));
     $arr = $st->fetch();
 
     if($arr)
-        if(password_verify($password, $arr['mdp'])) {  
-            $_SESSION['id']    = $arr['id'];         
+        if($password == $arr['mdp']) {
+            $_SESSION['id'] = $arr['id_user'];
         }
-
         header('Location: index.php');
     }
     ?>
